75118766aacaa2b2ad4384459e76b62defe9484a
Luis-Lopez12 <a24110228@live.ceti.mx>
2025-03-28 02:00:31 +0000

    practica 1

---
+++ Serie.hpp
@@ -0,0 +1,11 @@
+#include <Foco.hpp>
+#include <iostream>
+class Serie
+{
+private:
+    Foco focos[10];
+public:
+    Serie(/* args */) {}
+    ~Serie() {}
+    void EncenderTodo()
+};
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Full()
+    );
+
+    auto document=hbox({
+
+        vbox()
+    });
+    ftxui::Render(pantalla,document);
+    return EXIT_SUCCESS
+}
